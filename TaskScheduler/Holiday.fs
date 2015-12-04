f69232fc730e2915681f345053e70f7cb58438f1
adacola <adacola255@gmail.com>
2015-12-04 19:23:26 +0900

    祝日モジュールとスケジュール計算モジュールを追加

---
+++ TaskScheduler/Holiday.fs
@@ -0,0 +1,59 @@
+﻿namespace Adacola.TaskScheduler
+
+module Holiday =
+    open System
+    
+    type HolidayType =
+        | Fixed of month : int * day : int
+        | Fluctuation of month : int * weekOfMonth : int * dayOfWeek : DayOfWeek
+        | VernalEquinoxDay
+        | AutumnalEquinoxDay
+
+    let getDate year holidayType =
+        let getEquinoxDay start year = start + 0.242194 * float (year - 1980) - float ((year - 1980) / 4) |> int
+        let getVernalEquinoxDay year = DateTime(year, 3, getEquinoxDay 20.8431 year)
+        let getAutumnalEquinoxDay year = DateTime(year, 9, getEquinoxDay 23.2488 year)
+
+        match holidayType with
+        | Fixed(month, day) -> DateTime(year, month, day)
+        | Fluctuation(month, weekOfMonth, dayOfWeek) ->
+            [for i in -6 .. 0 -> DateTime(year, month, weekOfMonth * 7 + i)]
+            |> List.find (fun x -> x.DayOfWeek = dayOfWeek)
+        | VernalEquinoxDay -> getVernalEquinoxDay year
+        | AutumnalEquinoxDay -> getAutumnalEquinoxDay year
+
+    let japaneseHolidayTypes = [
+        Fixed(1, 1)
+        Fluctuation(1, 2, DayOfWeek.Monday)
+        Fixed(2, 11)
+        VernalEquinoxDay
+        Fixed(4, 29)
+        Fixed(5, 3)
+        Fixed(5, 4)
+        Fixed(5, 5)
+        Fluctuation(7, 3, DayOfWeek.Monday)
+        Fixed(8, 11)
+        Fluctuation(9, 3, DayOfWeek.Monday)
+        AutumnalEquinoxDay
+        Fluctuation(10, 2, DayOfWeek.Monday)
+        Fixed(11, 3)
+        Fixed(11, 23)
+        Fixed(12, 23)
+    ]
+        
+    let getJapaneseHolidays year =
+        let rec getSubstituteHolidays result = function
+            | [] -> result |> List.rev
+            | (holiday : DateTime)::holidays when holiday.DayOfWeek = DayOfWeek.Sunday ->
+                let substituteHoliday =
+                    Seq.initInfinite (fun i -> holiday.AddDays(float i + 1.0))
+                    |> Seq.find (fun d -> holidays |> List.forall ((<>) d))
+                getSubstituteHolidays (substituteHoliday::result) holidays
+            | _::holidays -> getSubstituteHolidays result holidays
+
+        let getNationalHolidays holidays =
+            holidays |> List.pairwise |> List.choose (function
+                | (x : DateTime), y when y - x = TimeSpan(2, 0, 0, 0) -> x.AddDays 1.0 |> Some
+                | _ -> None)
+        let holidays = japaneseHolidayTypes |> List.map (getDate year)
+        holidays @ getSubstituteHolidays [] holidays @ getNationalHolidays holidays
---
+++ TaskScheduler/Schedule.fs
@@ -0,0 +1,54 @@
+﻿namespace Adacola.TaskScheduler
+
+open System
+
+[<Measure>] type Hour
+[<Measure>] type Day
+
+type DateHour = { Date : DateTime; Hour : int<Hour> }
+type Task = { Name : string; Workload : decimal<Day> }
+type Schedule = { Task : Task; Start : DateHour; End : DateHour }
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Schedule =
+
+    /// <summary>
+    /// スケジュールを計算します。
+    /// </summary>
+    /// <param name="specialHolidays">土日以外の祝日、休日の集合</param>
+    /// <param name="weight">作業に取り組める割合</param>
+    /// <param name="startDate">作業開始日</param>
+    /// <param name="tasks">作業リスト。先に行うタスクから並んでいる前提です。</param>
+    /// <returns>スケジュールのリスト</returns>
+    let calculate specialHolidays weight (startDate : DateTime) tasks =
+        let add specialHolidays { Date = startDate; Hour = startHour } (duration : decimal<Day>) =
+            // TODO O(n)をO(1)に直せる
+            let rec add (date : DateTime) = function
+                | 0<Day> -> date
+                | n ->
+                    let d = date.AddDays 1.0
+                    let diff =
+                        if d.DayOfWeek = DayOfWeek.Saturday || d.DayOfWeek = DayOfWeek.Sunday || Set.contains d specialHolidays
+                        then 0<Day> else 1<Day>
+                    n - diff |> add d 
+
+            let durationDayDecimal = decimal duration |> floor |> (*) 1M<Day>
+            let durationHour = (duration - durationDayDecimal) * 8M |> decimal |> int |> (*) 1<Hour>
+            let durationDay = durationDayDecimal |> decimal |> int |> (*) 1<Day>
+
+            let addDay, endHour = (startHour + durationHour) / 8<Hour/Day> + durationDay, (startHour + durationHour) % 8<Hour>
+            let endDate = add startDate addDay
+            { Date = endDate; Hour = endHour }
+
+        (({ Date = startDate.Date; Hour = 0<Hour> }, []), tasks) ||> Seq.fold (fun (dateHour, schedules) task ->
+            let workload = task.Workload / weight
+            let endDateHour = add specialHolidays dateHour workload
+            endDateHour, { Task = task; Start = dateHour; End = endDateHour }::schedules)
+        |> snd |> List.rev
+
+    /// スケジュールのリストをTSV形式の文字列に変換します。
+    let toTsv schedules =
+        let dToS (d : DateTime) = sprintf "%04d/%02d/%02d" d.Year d.Month d.Day
+        schedules |> Seq.map (fun r ->
+            sprintf "%s\t%O\t%s\t%s" r.Task.Name r.Task.Workload (dToS r.Start.Date) (dToS r.End.Date))
+        |> String.concat "\r\n"
